38e6e853dfe159b1853baeb0cb7fb4d65e944e7d
Fabio Kreis <fabiokreis@gmail.com>
2020-03-17 07:19:42 -0300

    Added Fragments and navigation setup

---
+++ app/src/main/java/br/com/fabiokreis/dogs/view/ListFragment.kt
@@ -0,0 +1,21 @@
+package br.com.fabiokreis.dogs.view
+
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+
+import br.com.fabiokreis.dogs.R
+
+class ListFragment : Fragment() {
+
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        // Inflate the layout for this fragment
+        return inflater.inflate(R.layout.fragment_list, container, false)
+    }
+
+}
