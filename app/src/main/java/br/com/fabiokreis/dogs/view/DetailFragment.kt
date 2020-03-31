4cadbbed9fc068611de5bf7769f74472563bc254
Fabio Kreis <fabiokreis@gmail.com>
2020-03-31 08:12:38 -0300

    Added SMS and Share features

--- app/src/main/java/br/com/fabiokreis/dogs/view/DetailFragment.kt
+++ app/src/main/java/br/com/fabiokreis/dogs/view/DetailFragment.kt
@@ -1,11 +1,13 @@
 package br.com.fabiokreis.dogs.view
 
+import android.app.PendingIntent
+import android.content.Intent
 import android.graphics.Bitmap
 import android.graphics.drawable.Drawable
 import android.os.Bundle
-import android.view.LayoutInflater
-import android.view.View
-import android.view.ViewGroup
+import android.telephony.SmsManager
+import android.view.*
+import androidx.appcompat.app.AlertDialog
 import androidx.databinding.DataBindingUtil
 import androidx.fragment.app.Fragment
 import androidx.lifecycle.Observer
@@ -13,7 +15,10 @@ import androidx.lifecycle.ViewModelProviders
 import androidx.palette.graphics.Palette
 import br.com.fabiokreis.dogs.R
 import br.com.fabiokreis.dogs.databinding.FragmentDetailBinding
+import br.com.fabiokreis.dogs.databinding.SendSmsDialogBinding
+import br.com.fabiokreis.dogs.model.DogBreed
 import br.com.fabiokreis.dogs.model.DogPalette
+import br.com.fabiokreis.dogs.model.SmsInfo
 import br.com.fabiokreis.dogs.viewmodel.DetailViewModel
 import com.bumptech.glide.Glide
 import com.bumptech.glide.request.target.CustomTarget
@@ -25,11 +30,14 @@ class DetailFragment : Fragment() {
     private var dogUuid = 0
 
     private lateinit var dataBinding: FragmentDetailBinding
+    private var sendSmsStarted = false
+    private var currentDog: DogBreed? = null
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
+        setHasOptionsMenu(true)
         dataBinding = DataBindingUtil.inflate(inflater, R.layout.fragment_detail, container, false)
         return dataBinding.root
     }
@@ -50,6 +58,7 @@ class DetailFragment : Fragment() {
     private fun observeViewModel() {
         viewModel.dogLiveData.observe(this, Observer { dog ->
             dog?.let {
+                currentDog = dog
                 dataBinding.dog = dog
 
                 it.imageUrl?.let {
@@ -63,12 +72,12 @@ class DetailFragment : Fragment() {
         Glide.with(this)
             .asBitmap()
             .load(url)
-            .into(object : CustomTarget<Bitmap>(){
-                override fun onLoadCleared(placeholder: Drawable?) { }
+            .into(object : CustomTarget<Bitmap>() {
+                override fun onLoadCleared(placeholder: Drawable?) {}
 
                 override fun onResourceReady(resource: Bitmap, transition: Transition<in Bitmap>?) {
                     Palette.from(resource)
-                        .generate { palette ->  
+                        .generate { palette ->
                             val intColor = palette?.lightMutedSwatch?.rgb ?: 0
                             val myPalette = DogPalette(intColor)
                             dataBinding.palette = myPalette
@@ -76,6 +85,68 @@ class DetailFragment : Fragment() {
                 }
 
             })
+    }
+
+    override fun onCreateOptionsMenu(menu: Menu, inflater: MenuInflater) {
+        super.onCreateOptionsMenu(menu, inflater)
+        inflater.inflate(R.menu.detail_menu, menu)
+    }
+
+    override fun onOptionsItemSelected(item: MenuItem): Boolean {
+        when (item.itemId) {
+            R.id.action_send_sms -> {
+                sendSmsStarted = true
+                (activity as MainActivity).checkSmsPermission()
+
+            }
+            R.id.action_share -> {
+                val intent = Intent(Intent.ACTION_SEND)
+                intent.type = "text/plain"
+                intent.putExtra(Intent.EXTRA_SUBJECT, "checkout this dog breed")
+                intent.putExtra(Intent.EXTRA_TEXT, "${currentDog?.dogBreed} bred for ${currentDog?.bredFor}")
+                intent.putExtra(Intent.EXTRA_STREAM, currentDog?.imageUrl)
+                startActivity(Intent.createChooser(intent, "Share with"))
+            }
+        }
+        return super.onOptionsItemSelected(item)
+    }
+
+    fun onPermissionResult(permissionGranted: Boolean) {
+        if (sendSmsStarted && permissionGranted) {
+            context?.let {
+                val smsInfo = SmsInfo(
+                    "", "${currentDog?.dogBreed} bred for ${currentDog?.bredFor}",
+                    currentDog?.imageUrl
+                )
+
+                val dialogBiding = DataBindingUtil.inflate<SendSmsDialogBinding>(
+                    LayoutInflater.from(it),
+                    R.layout.send_sms_dialog,
+                    null,
+                    false
+                )
+
+                AlertDialog.Builder(it)
+                    .setView(dialogBiding.root)
+                    .setPositiveButton("Send SMS") { dialog, which ->
+                        if (!dialogBiding.smsDestination.text.isNullOrEmpty()) {
+                            smsInfo.to = dialogBiding.smsDestination.text.toString()
+                            sendSms(smsInfo)
+                        }
+                    }
+                    .setNegativeButton("Cancel") {dialog, which ->  }
+                    .show()
+
+                dialogBiding.smsInfo = smsInfo
+            }
+        }
+    }
+
+    private fun sendSms(smsInfo: SmsInfo) {
+        val intent = Intent(context, MainActivity::class.java)
+        val pi = PendingIntent.getActivity(context, 0, intent, 0)
+        val sms = SmsManager.getDefault()
+        sms.sendTextMessage(smsInfo.to, null, smsInfo.text, pi, null)
     }
 
 }
